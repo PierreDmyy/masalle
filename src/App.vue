e8541bc0fa380e75616b71662e4afefed051a797
Pierre Dumay <pierredumay@air-de-pierre.home>
2024-02-06 20:56:33 +0100

    add color and clean code

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,85 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+const currentTime = ref('');
+const currentDate = ref('');
+
+
+// Mettre à jour l'heure actuelle
+setInterval(() => {
+  const now = new Date();
+  currentDate.value = now.toLocaleDateString('fr-FR', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+  currentTime.value = now.toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+}, 1000);
 </script>
 
 <template>
   <div>
     <header>
       <img class="logo" src="@/assets/logoCFP02.png"></img>
-      <h1 class="title">Localiser ma salle :</h1>
+      <p class="date">{{ currentDate }}</p> <!-- Afficher la date ici -->
+      <p class="time">{{ currentTime }}</p> <!-- Afficher l'heure ici -->
     </header>
     <HelloWorld />
   </div>
 </template>
 
+
+
 <style scoped>
-  header {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    padding: 10px;
-    background-color: #fff;
-    z-index: 1000;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-    display: flex;
-    justify-content: flex-end; /* Pour aligner les éléments à droite */
-    align-items: center; /* Pour centrer verticalement les éléments */
-  }
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap');
 
-  .logo {
-    height: 100px;
-    width: auto;
-    margin-right: auto; /* Pour pousser le titre à droite */
-  }
+header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  padding: 10px;
+  background-color: #fff;
+  z-index: 1000;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  display: flex;
+  justify-content: space-between; /* Pour aligner les éléments aux extrémités */
+  align-items: center; /* Pour centrer verticalement les éléments */
+}
 
-  .title {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    margin: 0; /* Supprimer les marges par défaut */  }
+.logo {
+  height: 100px;
+  width: auto;
+}
 
-  nav a.router-link-exact-active {
-    color: var(--color-text);
-  }
+.time {
+  font-family: 'Poppins', sans-serif;
+  margin: 20px;
+  font-size: 25px;
+  color: blue;
+}
+.date{
+  font-family: 'Poppins', sans-serif;
+  margin: 0;
+  font-size: 25px;
+  color: blue;
+}
 
-  nav a.router-link-exact-active:hover {
-    background-color: transparent;
-  }
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
 
-  nav a {
-    display: inline-block;
-    padding: 0 1rem;
-    border-left: 1px solid var(--color-border);
-  }
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
 
-  nav a:first-of-type {
-    border: 0;
-  }
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
 </style>
+
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -23,11 +23,21 @@ export default {
       // Mapping des noms de salle aux couleurs correspondantes
       const colorMap = {
         'Salle A': '#ffcccc', 
-        'Salle Remparts (Jaune)': '#ffff38', 
-        'Salle Cordelier (Vert pâle)': '#8fffbf', 
+        'Salle Remparts (Jaune)': 'rgba(250, 241, 29, 0.9)', 
+        'Salle Cordelier (Vert pâle)': '#99cc99', 
         'Salle Chatelaine (bleue)': '#3240a8' 
       };
       return colorMap[res_name] || '#D9FDFD'; // Couleur par défaut si aucun mapping trouvé
+    },
+    getTextColor(res_name) {
+      // Mapping des noms de salle aux couleurs correspondantes
+      const colorMap = {
+        'Salle A': '#ffcccc', 
+        'Salle Remparts (Jaune)': '#003681', 
+        'Salle Cordelier (Vert pâle)': '#003681', 
+        'Salle Chatelaine (bleue)': '#ffff' 
+      };
+      return colorMap[res_name] || '#003681'; // Couleur par défaut si aucun mapping trouvé
     }
   },
 };
@@ -39,10 +49,10 @@ export default {
     <!-- Body -->
     <main>
       <div class="grid-container">
-        <div class="card" v-for="room in inforooms" :key="room.id" :style="{ backgroundColor: getCardBackgroundColor(room.res_name) }">
+        <div class="card" v-for="room in inforooms" :key="room.id" :style="{ backgroundColor: getCardBackgroundColor(room.res_name), color: getTextColor(room.res_name) }">
           <p class="roomP">{{ room.description }} </p>
           <p class="roomP">{{ room.res_name }}</p>
-          <p>Avec : {{ room.full_name }}</p>
+          <p class="Person">Avec : {{ room.full_name }}</p>
         </div>
       </div>
     </main>
@@ -50,6 +60,8 @@ export default {
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap');
+
 .grid-container {
   display: grid;
   grid-template-columns: repeat(4, 1fr); /* 4 cartes par ligne */
@@ -85,6 +97,7 @@ main {
 }
 
 h1 {
+  font-family: 'Poppins', sans-serif;
   color: #003482;
   font-weight: bold;
   font-size: 40px;
@@ -101,7 +114,15 @@ p {
 }
 
 .roomP {
-  color: #040404;
+  font-family: 'Poppins', sans-serif;
+  align-items: center;
+  text-align: center;
+  font-weight: 900;
+  font-size: 25px;
+}
+.Person{
+  align-items: center;
+  text-align: center;
   font-weight: 900;
   font-size: 20px;
 }
@@ -110,8 +131,10 @@ p {
   width: 350px;
   height: auto;
   color: #003482;
-  border: 7px solid;
-  padding: 20px;
+  border: solid 5px #250f0f36;
+  box-shadow: rgba(17, 17, 26, 0.1) 0px 0px 16px;
+  border-radius: 3%;
+ padding: 20px;
   margin: -3px;
 }
 
